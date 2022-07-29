8379ae7ab9a4810d5576f73c1b0efa8f00387bc2
Lawrence Qupty <lawrencequp@gmail.com>
2022-07-29 15:20:03 -0700

    Converted Emacs config to an org file which will now be updated.

--- init.el
+++ init.el
@@ -1,45 +1,14 @@
 (defvar vars/default-font-size 130)
 
+;; set authentication tokens file
+(setq auth-sources '("~/.authinfo"))
+
 (setq backup-directory-alist `(("." . "~/.saves")))
 (setq backup-by-copying t)
 
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
 
-(setq inhibit-startup-screen t)
-(scroll-bar-mode -1)        ; Disable visible scrollbar
-(tool-bar-mode -1)          ; Disable thetoolbr
-(tooltip-mode -1)           ; Disable tooltips
-(set-fringe-mode 10)        ; Give some breathing room
-(menu-bar-mode -1)
-
-;; set authentication tokens file
-(setq auth-sources '("~/.authinfo"))
-
-
-(set-face-attribute 'default nil
-		    :font "JetBrains Mono"
-		    :weight 'light
-		    :height vars/default-font-size)
-
-
-;; Set the fixed pitch face
-(set-face-attribute 'fixed-pitch nil :font "JetBrains Mono" :height 140)
-
-;; Set the variable pitch face
-(set-face-attribute 'variable-pitch nil :font "DejaVu Sans" :height 180 :weight 'regular)
-
-(global-display-line-numbers-mode t)
-(menu-bar--display-line-numbers-mode-relative)
-
-;; Enable line numbers for some modes
-(dolist (mode '(org-mode-hook
-                term-mode-hook
-                eshell-mode-hook
-		shell-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
-
-;; Initialize package sources
 (require 'package)
 
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
@@ -50,11 +19,12 @@
 (unless package-archive-contents
  (package-refresh-contents))
 
-;; Initialize use-package on non-Linux platforms
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 
-;; Install straight.el
+(require 'use-package)
+(setq use-package-always-ensure t)
+
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
@@ -68,31 +38,31 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
-(require 'use-package)
-(setq use-package-always-ensure t)
+(setq inhibit-startup-screen t)
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable thetoolbr
+(tooltip-mode -1)           ; Disable tooltips
+(set-fringe-mode 10)        ; Give some breathing room
+(menu-bar-mode -1)
 
-(use-package ivy
-  :diminish
-  :bind (("C-s" . swiper)
-         :map ivy-minibuffer-map
-         ("TAB" . ivy-alt-done)
-         ("C-l" . ivy-alt-done)
-         ("C-j" . ivy-next-line)
-         ("C-k" . ivy-previous-line)
-         :map ivy-switch-buffer-map
-         ("C-k" . ivy-previous-line)
-         ("C-l" . ivy-done)
-         ("C-d" . ivy-switch-buffer-kill)
-         :map ivy-reverse-i-search-map
-         ("C-k" . ivy-previous-line)
-         ("C-d" . ivy-reverse-i-search-kill))
-  :init (ivy-mode 1))
+(global-display-line-numbers-mode t)
+(menu-bar--display-line-numbers-mode-relative)
 
-;; NOTE: The first time you load your configuration on a new machine, you'll
-;; need to run the following command interactively so that mode line icons
-;; display correctly:
-;;
-;; M-x all-the-icons-install-fonts
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                eshell-mode-hook
+                shell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+(set-face-attribute 'default nil
+		    :font "JetBrains Mono"
+		    :weight 'light
+		    :height vars/default-font-size)
+
+(set-face-attribute 'fixed-pitch nil :font "JetBrains Mono" :height 140)
+
+(set-face-attribute 'variable-pitch nil :font "DejaVu Sans" :height 180 :weight 'regular)
 
 (use-package all-the-icons)
 
@@ -111,46 +81,13 @@
   ;; Enable custom neotree theme (all-the-icons must be installed!)
   (doom-themes-neotree-config)
   ;; or for treemacs users
-  (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
-  (doom-themes-treemacs-config)
+  ;; use "doom-colors" for less minimal icon theme
+  (setq doom-themes-treemacs-theme "doom-atom")    (doom-themes-treemacs-config)
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
 
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
-
-(use-package which-key
-  :init (which-key-mode)
-  :diminish which-key-mode
-  :config
-  (setq which-key-idle-delay 0.3))
-
-(use-package ivy-rich
-  :init (ivy-rich-mode 1))
-
-;; dont inherit faces from mode
-(with-eval-after-load 'ivy-faces
-  (set-face-attribute 'ivy-org nil :inherit 'default))
-
-(use-package counsel
-  :bind (("M-x" . counsel-M-x)
-	 ("C-x b" . counsel-ibuffer)
-         ("C-x C-f" . counsel-find-file)
-	 :map minibuffer-local-map
-	 ("C-r" . counsel-minibuffer-history))
-  :config
-  (setq ivy-initial-inputs-alist nil)) ;; Don't start searches with ^
-
-(use-package helpful
-  :custom
-  (counsel-describe-function-function #'helpful-callable)
-  (counsel-describe-variable-function #'helpful-variable)
-  :bind
-  ([remap describe-function] . counsel-describe-function)
-  ([remap describe-symbol] . helpful-symbol)
-  ([remap describe-variable] . counsel-describe-variable)
-  ([remap describe-command] . helpful-command)
-  ([remap describe-key] . helpful-key))
 
 (use-package evil
   :init
@@ -161,10 +98,12 @@
   (setq evil-want-fine-undo t) 
   :config
   (evil-mode 1)
-  (define-key evil-insert-state-map (kbd "C-SPC") 'evil-delete-backward-char-and-join)
+  (define-key evil-insert-state-map (kbd "C-SPC")
+    'evil-delete-backward-char-and-join)
   (define-key evil-motion-state-map (kbd ":") 'evil-repeat-find-char)
   (define-key evil-motion-state-map (kbd ";") 'evil-ex)
-  (define-key evil-motion-state-map (kbd "H") 'evil-first-non-blank-of-visual-line)
+  (define-key evil-motion-state-map (kbd "H")
+    'evil-first-non-blank-of-visual-line)
   (define-key evil-motion-state-map (kbd "L") 'evil-end-of-visual-line)
   ;; vim like window movement
   (define-key global-map (kbd "C-h") 'evil-window-left)
@@ -178,11 +117,12 @@
   (evil-set-initial-state 'messages-buffer-mode 'normal)
   (evil-set-initial-state 'dashboard-mode 'normal))
 
-
 (use-package evil-collection
   :after evil
   :config
   (evil-collection-init))
+
+(use-package general)
 
 (use-package projectile
   :diminish projectile-mode
@@ -203,108 +143,149 @@
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
-(use-package general)
+(general-define-key
+   :keymaps 'transient-base-map
+   "<escape>" 'transient-quit-one)
+
+(use-package forge)
+
+(use-package ivy
+    :diminish
+    :bind (("C-s" . swiper)
+           :map ivy-minibuffer-map
+           ("TAB" . ivy-alt-done)
+           ("C-l" . ivy-alt-done)
+           ("C-j" . ivy-next-line)
+           ("C-k" . ivy-previous-line)
+           :map ivy-switch-buffer-map
+           ("C-k" . ivy-previous-line)
+           ("C-l" . ivy-done)
+           ("C-d" . ivy-switch-buffer-kill)
+           :map ivy-reverse-i-search-map
+           ("C-k" . ivy-previous-line)
+           ("C-d" . ivy-reverse-i-search-kill))
+    :init (ivy-mode 1))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         :map minibuffer-local-map
+         ("C-r" . counsel-minibuffer-history))
+  :config
+  (setq ivy-initial-inputs-alist nil)) ;; Don't start searches with ^
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+(use-package ivy-rich
+  :init (ivy-rich-mode 1))
+
+(with-eval-after-load 'ivy-faces
+  (set-face-attribute 'ivy-org nil :inherit 'default))
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-symbol] . helpful-symbol)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-key] . helpful-key))
 
 (general-define-key
  "<escape>" 'keyboard-escape-quit
  "C-M-j" 'counsel-switch-buffer)
 
-;; escape magit popups with esc
-(general-define-key
-   :keymaps 'transient-base-map
-   "<escape>" 'transient-quit-one)
-
-;; NOTE: Make sure to configure a GitHub token before using this package!
-;; - https://magit.vc/manual/forge/Token-Creation.html#Token-Creation
-;; - https://magit.vc/manual/ghub/Getting-Started.html#Getting-Started
-(use-package forge)
-
-;; Org Mode Configuration ------------------------------------------------------
-
 (defun lq/org-font-setup ()
-  ;; Replace list hyphen with dot
-  (font-lock-add-keywords 'org-mode
-                          '(("^ *\\([-]\\) "
-                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
+    ;; Replace list hyphen with dot
+    (font-lock-add-keywords 'org-mode
+                            '(("^ *\\([-]\\) "
+                               (0 (prog1 ()
+(compose-region (match-beginning 1) (match-end 1) "•"))))))
 
-  ;; Set faces for heading levels
-  (dolist (face '((org-level-1 . 1.2)
-                  (org-level-2 . 1.1)
-                  (org-level-3 . 1.05)
-                  (org-level-4 . 1.0)
-                  (org-level-5 . 1.1)
-                  (org-level-6 . 1.1)
-                  (org-level-7 . 1.1)
-                  (org-level-8 . 1.1)))
-    (set-face-attribute (car face) nil :font "DejaVu Sans" :weight 'regular :height (cdr face)))
+    ;; Set faces for heading levels
+    (dolist (face '((org-level-1 . 1.2)
+                    (org-level-2 . 1.1)
+                    (org-level-3 . 1.05)
+                    (org-level-4 . 1.0)
+                    (org-level-5 . 1.1)
+                    (org-level-6 . 1.1)
+                    (org-level-7 . 1.1)
+                    (org-level-8 . 1.1)))
+      (set-face-attribute (car face) nil
+                          :font "DejaVu Sans" :weight 'regular :height (cdr face)))
 
-  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
-  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
-  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
-
-(with-eval-after-load 'org-indent
-  (set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch)))
+    ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+    (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+    (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+    (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
+    (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+    (set-face-attribute 'org-special-keyword nil
+                        :inherit '(font-lock-comment-face fixed-pitch))
+    (set-face-attribute 'org-meta-line nil
+                        :inherit '(font-lock-comment-face fixed-pitch))
+    (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
 
 (defun lq/org-mode-setup ()
+  (setq org-ellipsis " ▾")
   (org-indent-mode)
   (variable-pitch-mode 1)
   (visual-line-mode 1)
-  (add-hook 'org-mode-hook (lambda () (display-line-numbers-mode -1))))
+  (display-line-numbers-mode -1))
 
 (use-package org
   :hook (org-mode . lq/org-mode-setup)
   :config
-  
-  (setq org-refile-targets
-    '(("archive.org" :maxlevel . 1)
-      ("todo.org" :maxlevel . 1)))
-
-  ;; Save Org buffers after refiling!
-  (advice-add 'org-refile :after 'org-save-all-org-buffers)
-  
-  (setq org-ellipsis " ▾")
-
-  (setq org-agenda-files '("~/Documents/Org"))
-  (setq org-agenda-start-with-log-mode t)
-  (setq org-log-done 'time)
-  (setq org-log-into-drawer t)
-  
-  (setq org-capture-templates
-	`(("t" "Tasks / Projects")
-	  ("tt" "Task" entry (file+olp "~/Documents/Org/todo.org" "Inbox")
-           "* TODO %?\n  %U\n  %a\n  %i" :empty-lines 1)
-
-	  ("j" "Journal Entries")
-	  ("jj" "Journal" entry
-           (file+olp+datetree "~/Documents/Org/journal.org")
-           "\n* %<%I:%M %p> - Journal :journal:\n\n%?\n\n"
-           ;; ,(dw/read-file-as-string "~/Notes/Templates/Daily.org")
-           :clock-in :clock-resume
-           :empty-lines 1)
-	  ("jm" "Meeting" entry
-           (file+olp+datetree "~/Documents/Org/journal.org")
-           "* %<%I:%M %p> - %a :meetings:\n\n%?\n\n"
-           :clock-in :clock-resume
-           :empty-lines 1)
-
-	  ("w" "Workflows")
-	  ("we" "Checking Email" entry (file+olp+datetree "~/Documents/Org/journal.org")
-           "* Checking Email :email:\n\n%?" :clock-in :clock-resume :empty-lines 1)
-
-	  ("m" "Metrics Capture")
-	  ("mw" "Weight" table-line (file+headline "~/Documents/Org/stats.org" "Weight")
-	   "| %U | %^{Weight} | %^{Notes} |" :kill-buffer t)))
-
-  (define-key global-map (kbd "C-c c")
-    (lambda () (interactive) (org-capture nil)))
-  
   (lq/org-font-setup))
 
+(setq org-refile-targets
+      '(("archive.org" :maxlevel . 1)
+        ("todo.org" :maxlevel . 1)))
+
+(advice-add 'org-refile :after 'org-save-all-org-buffers)
+
+(setq org-agenda-files '("~/Documents/Org"))
+(setq org-agenda-start-with-log-mode t)
+(setq org-log-done 'time)
+(setq org-log-into-drawer t)
+
+(setq org-capture-templates
+      `(("t" "Tasks / Projects")
+        ("tt" "Task" entry (file+olp "~/Documents/Org/todo.org" "Inbox")
+         "* TODO %?\n  %U\n  %a\n  %i" :empty-lines 1)
+
+        ("j" "Journal Entries")
+        ("jj" "Journal" entry
+         (file+olp+datetree "~/Documents/Org/journal.org")
+         "\n* %<%I:%M %p> - Journal :journal:\n\n%?\n\n"
+         ;; ,(dw/read-file-as-string "~/Notes/Templates/Daily.org")
+         :clock-in :clock-resume
+         :empty-lines 1)
+        ("jm" "Meeting" entry
+         (file+olp+datetree "~/Documents/Org/journal.org")
+         "* %<%I:%M %p> - %a :meetings:\n\n%?\n\n"
+       :clock-in :clock-resume
+       :empty-lines 1)
+
+        ("w" "Workflows")
+        ("we" "Checking Email" entry
+         (file+olp+datetree "~/Documents/Org/journal.org")
+         "* Checking Email :email:\n\n%?"
+         :clock-in :clock-resume :empty-lines 1)
+
+        ("m" "Metrics Capture")
+        ("mw" "Weight" table-line
+         (file+headline "~/Documents/Org/stats.org" "Weight")
+         "| %U | %^{Weight} | %^{Notes} |" :kill-buffer t)))
+
+(define-key global-map (kbd "C-c c")
+  (lambda () (interactive) (org-capture nil)))
 
 (use-package org-bullets
   :after org
@@ -317,18 +298,19 @@
         visual-fill-column-center-text t)
   (visual-fill-column-mode 1))
 
+(use-package visual-fill-column
+  :hook (org-mode . lq/org-mode-visual-fill))
+
 (add-hook 'org-mode-hook (lambda ()
-   "Beautify Org Checkbox Symbol"
    (push '("[ ]" . "☐") prettify-symbols-alist)
    (push '("[X]" . "☑" ) prettify-symbols-alist)
    (push '("[-]" . "❍" ) prettify-symbols-alist)
    (prettify-symbols-mode)))
 
-(use-package visual-fill-column
-  :hook (org-mode . lq/org-mode-visual-fill))
+(with-eval-after-load 'org-indent
+  (set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch)))
 
 (defun org-toggle-emphasis ()
-  "Toggle hiding/showing of org emphasize markers."
   (interactive)
   (if org-hide-emphasis-markers
       (set-variable 'org-hide-emphasis-markers nil)
@@ -343,3 +325,25 @@
   :straight (:host github
              :repo "tecosaur/org-pandoc-import"
              :files ("*.el" "filters" "preprocessors")))
+
+(require 'org-tempo)
+
+(add-to-list 'org-structure-template-alist '("sh" . "src sh"))
+(add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+
+(org-babel-do-load-languages
+  'org-babel-load-languages
+  '((emacs-lisp . t)
+    (python . t)))
+
+(setq org-confirm-babel-evaluate nil)
+
+(defun efs/org-babel-tangle-config ()
+  (when (string-equal (buffer-file-name)
+                      (expand-file-name "~/configRepo/config/Emacs.org"))
+    ;; Dynamic scoping to the rescue
+    (let ((org-confirm-babel-evaluate nil))
+      (org-babel-tangle))))
+
+(add-hook 'org-mode-hook
+          (lambda () (add-hook 'after-save-hook #'efs/org-babel-tangle-config)))
