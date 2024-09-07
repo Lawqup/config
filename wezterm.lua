e6e7ff7d9cf6bc189223160e8ac0747d81d3d47d
Lawrence Qupty <lawqup@amazon.com>
2024-09-07 14:50:55 -0700

    Mac stuff

---
+++ wezterm.lua
@@ -0,0 +1,11 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- For example, changing the color scheme:
+config.color_scheme = 'nord'
+
+-- and finally, return the configuration to wezterm
+return config
