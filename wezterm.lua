7172b00209d29a9d5ecc3cdfd178df874139c264
Lawrence Qupty <lawqup@amazon.com>
2024-11-30 23:36:42 -0800

    update wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -1,5 +1,12 @@
+local wezterm = require 'wezterm'
+local act = wezterm.action
+
 return {
-   color_scheme = 'Catppuccin Mocha',
-   window_decorations = "RESIZE",
-   enable_tab_bar = false
+	color_scheme = 'Catppuccin Mocha',
+	window_decorations = "RESIZE",
+	enable_tab_bar = false,
+	keys = {
+		{ key = "p",   mods = "CTRL", action = act.ScrollToPrompt(-1) },
+		{ key = "n", mods = "CTRL", action = act.ScrollToPrompt(1) },
+	},
 }
